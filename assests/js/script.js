d41ac08158b35537a18f9a2e0dec089ca4733a6a
asmahu <asma.hussaini2018@gmail.com>
2022-07-08 02:54:06 +0000

    create function to start the game

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,4 +3,25 @@ const gameArea = () => {
     let playerScr = 0;
     let computerScr = 0;
     let moves = 0;
+     // Function to
+  const playGame = () => {
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorButton = document.querySelector('.scissor');
+    const playerOpt = [rockButton,paperButton,scissorButton];
+    const computerOpt = ['rock','paper','scissors']
+     // Function to start playing game
+     playerOpt.forEach(option => {
+        option.addEventListener('click',function(){
+  
+          const movesLeft = document.querySelector('.movesleft');
+          moves++;
+          movesLeft.innerText = `Moves Left: ${10-moves}`;
+          
+  
+          const choiceNum = Math.floor(Math.random()*3);
+          const computerChoice = computerOpt[choiceNum];
+  
+          
+          
   

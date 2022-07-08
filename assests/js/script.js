fad73436bf1e6a367cf66e4523af1e89b873bbba
asmahu <asma.hussaini2018@gmail.com>
2022-07-08 03:02:14 +0000

    calling playgame funciton

--- assests/js/script.js
+++ assests/js/script.js
@@ -78,6 +78,91 @@ const gameArea = () => {
       }
     }
   }
+  // Function to run when game is over
+  const gameover = (playerOpt,movesLeft) => {
+
+    const chooseM = document.querySelector('.move');
+    const resu// Function to decide winner
+  const winner = (plyr,comp) => {
+    const result = document.querySelector('.result');
+    const playerScoreBoard = document.querySelector('.p-count');
+    const computerScoreBoard = document.querySelector('.c-count');
+    plyr = plyr.toLowerCase();
+    comp = comp.toLowerCase();
+    if(plyr === comp){
+      result.textContent = 'Tie'
+    }
+    else if(plyr == 'Rock'){
+      if(comp == 'Paper'){
+        result.textContent = 'Computer Won';
+        computerScr++;
+        computerScoreBoard.textContent = computerScr;
+
+      }else{
+        result.textContent = 'Player Won'
+        playerScr++;
+        playerScoreBoard.textContent = playerScr;
+      }
+    }
+    else if(plyr == 'scissors'){
+      if(comp == 'rock'){
+        result.textContent = 'Computer Won';
+        computerScr++;
+        computerScoreBoard.textContent = computerScr;
+      }else{
+        result.textContent = 'Player Won';
+        playerScr++;
+        playerScoreBoard.textContent = playerScr;
+      }
+    }
+    else if(plyr == 'paper'){
+      if(comp == 'scissors'){
+        result.textContent = 'Computer Won';
+        computerScr++;
+        computerScoreBoard.textContent = computerScr;
+      }else{
+        result.textContent = 'Player Won';
+        playerScr++;
+        playerScoreBoard.textContent = playerScr;
+      }
+    }
+  }
+lt = document.querySelector('.result');
+    const reloadButton = document.querySelector('.reload');
+
+    playerOpt.forEach(option => {
+      option.style.display = 'none';
+    })
+
+  
+    chooseM.innerText = 'Game Over!!'
+    movesLeft.style.display = 'none';
+
+    if(playerScr > computerScr){
+      result.style.fontSize = '2rem';
+      result.innerText = 'You Won The Game'
+      result.style.color = '#fff';
+    }
+    else if(playerScr < computerScr){
+      result.style.fontSize = '2rem';
+      result.innerText = 'You Lost The Game';
+      result.style.color = 'fff';
+    }
+    else{
+      result.style.fontSize = '2rem';
+      result.innerText = 'Tie';
+      result.style.color = '#fff'
+    }
+    reloadButton.innerText = 'Restart';
+    reloadButton.style.display = 'flex'
+    reloadButton.addEventListener('click',() => {
+      window.location.reload();
+    })
+  }
+// Calling playGame function
+playGame();
+  
+}
 
           
   

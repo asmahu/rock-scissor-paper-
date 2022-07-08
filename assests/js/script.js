a8c27819e9005a5f4a24e24b9facebb7d630a150
asmahu <asma.hussaini2018@gmail.com>
2022-07-08 03:14:12 +0000

    function to run when game is over

--- assests/js/script.js
+++ assests/js/script.js
@@ -82,52 +82,7 @@ const gameArea = () => {
   const gameover = (playerOpt,movesLeft) => {
 
     const chooseM = document.querySelector('.move');
-    const resu// Function to decide winner
-  const winner = (plyr,comp) => {
     const result = document.querySelector('.result');
-    const playerScoreBoard = document.querySelector('.p-count');
-    const computerScoreBoard = document.querySelector('.c-count');
-    plyr = plyr.toLowerCase();
-    comp = comp.toLowerCase();
-    if(plyr === comp){
-      result.textContent = 'Tie'
-    }
-    else if(plyr == 'Rock'){
-      if(comp == 'Paper'){
-        result.textContent = 'Computer Won';
-        computerScr++;
-        computerScoreBoard.textContent = computerScr;
-
-      }else{
-        result.textContent = 'Player Won'
-        playerScr++;
-        playerScoreBoard.textContent = playerScr;
-      }
-    }
-    else if(plyr == 'scissors'){
-      if(comp == 'rock'){
-        result.textContent = 'Computer Won';
-        computerScr++;
-        computerScoreBoard.textContent = computerScr;
-      }else{
-        result.textContent = 'Player Won';
-        playerScr++;
-        playerScoreBoard.textContent = playerScr;
-      }
-    }
-    else if(plyr == 'paper'){
-      if(comp == 'scissors'){
-        result.textContent = 'Computer Won';
-        computerScr++;
-        computerScoreBoard.textContent = computerScr;
-      }else{
-        result.textContent = 'Player Won';
-        playerScr++;
-        playerScoreBoard.textContent = playerScr;
-      }
-    }
-  }
-lt = document.querySelector('.result');
     const reloadButton = document.querySelector('.reload');
 
     playerOpt.forEach(option => {
@@ -159,6 +114,7 @@ lt = document.querySelector('.result');
       window.location.reload();
     })
   }
+
 // Calling playGame function
 playGame();
   
